8faa2009e221896daa6437514c3fa5fd59b6e22c
saneria <dapalkrizia@gmail.com>
2023-03-29 12:56:19 +0800

    first commit

---
+++ app/Http/Controllers/Api/CarouselItemsController.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Models\CarouselItems; 
+use App\Http\Controllers\Controller;
+use App\Http\Requests\CarouselItemsRequest;
+use Illuminate\Http\Request;
+
+
+
+class CarouselItemsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return CarouselItems::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(CarouselItemsRequest $request)
+    {
+
+        //Retrieve the validated input data
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::create($validated);
+
+        return $carouselItem;
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        return CarouselItems::findOrFail($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(CarouselItemsRequest $request, string $id)
+    {
+        $validated = $request->validated();
+
+        $carouselItem = CarouselItems::findOrFail($id);
+
+        $carouselItem->update($validated);
+
+        return $carouselItem;
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $carouselItem = CarouselItems::findOrFail($id);
+
+        $carouselItem->delete();
+
+        return $carouselItem;
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\CarouselItemsController;
+use App\Http\Controllers\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/carousel', [CarouselItemsController::class, 'index']);
+Route::get('/carousel/{id}', [CarouselItemsController::class, 'show']);
+Route::post('/carousel', [CarouselItemsController::class, 'store']);
+Route::put('/carousel/{id}', [CarouselItemsController::class, 'update']);
+Route::delete('/carousel/{id}', [CarouselItemsController::class, 'destroy']);
+
+Route::get('/user', [UserController::class, 'index']);
+Route::delete('/user/{id}', [UserController::class, 'destroy']);
